370cf3c0c4d3dde04848991d979f250c4cc8c994
lorithurman87 <lori_thurman@rocketmail.com>
2018-07-19 17:16:02 -0500

    Add controller and models package

---
+++ src/main/java/com/example/Pop/controllers/PopController.java
@@ -0,0 +1,10 @@
+package com.example.Pop.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("pop")
+public class PopController {
+}
