a5f34a0b40237000e1b277e2263615a29319f883
lorithurman87 <lori_thurman@rocketmail.com>
2018-07-20 20:33:02 -0500

    trying to start the project. added a hello world

--- src/main/java/com/example/Pop/controllers/PopController.java
+++ src/main/java/com/example/Pop/controllers/PopController.java
@@ -3,11 +3,14 @@ package com.example.Pop.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("pop")
+@RequestMapping(value="/pop", method = RequestMethod.GET)
 public class PopController {
 
-
+    public static void main(String[] args) {
+        System.out.println ("Hello and welcome to Pop");
+    }
     //return "home";
 }
