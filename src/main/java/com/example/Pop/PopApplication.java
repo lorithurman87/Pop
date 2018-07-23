f808651f2726401c9e53b4531b0b78c687c7fe57
lorithurman87 <lori_thurman@rocketmail.com>
2018-07-22 22:39:09 -0500

    added a login and sign up html, added a link to both html pages on home page

--- src/main/java/com/example/Pop/PopApplication.java
+++ src/main/java/com/example/Pop/PopApplication.java
@@ -2,11 +2,18 @@ package com.example.Pop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @SpringBootApplication
 public class PopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PopApplication.class, args);
+
 	}
+
 }
--- src/main/java/com/example/Pop/controllers/HelloController.java
+++ src/main/java/com/example/Pop/controllers/HelloController.java
@@ -2,15 +2,63 @@ package com.example.Pop.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/pop")
 public class HelloController {
 
-    @RequestMapping(value = "/pop", method = RequestMethod.GET)
-    public String home() {
-        return "home";
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model) {
+        String aBitAbout = "Pop (Plenty of pets) is an awesome new website with the goal to help you find your new best friend. Pop lets you know the details both\n" +
+                "    good and bad, about animals that are or will be seeking new homes to try to make better and ever lasting matches that will help both the adopter\n" +
+                "    and the animal. You can search for animals and add them to your favorites and then contact the rescue to get more information about setting up a\n" +
+                "    meet and greet. So what are you waiting for?.";
+
+        model.addAttribute("title", "Welcome to Pop!!");
+        model.addAttribute("aBitAbout",aBitAbout);
+        return "index";
+    }
+
+    @RequestMapping(value = "signup", method = RequestMethod.GET)
+    public String displaySignUpForm(Model model) {
+
+        model.addAttribute("title", "Welcome to Pop!!");
+        model.addAttribute("userName", "Username: ");
+        model.addAttribute("passWord", "Password: ");
+        model.addAttribute("eMail", "E-mail: ");
+        model.addAttribute("location", "Location: ");
+        return "Users/signUp";
+    }
+
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String displayLogInForm(Model model) {
+
+        model.addAttribute("title", "Welcome to Pop!!");
+        model.addAttribute("userName", "Username: ");
+        model.addAttribute("passWord", "Password: ");
+        return "Users/logIn";
+    }
+
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String processLogInForm(Model model) {
+
+        model.addAttribute("title", "Welcome to Pop!!");
+
+        return "";
+    }
+
+
+    @RequestMapping(value = "signup", method = RequestMethod.POST)
+    public String processSignUpForm(Model model) {
+
+        model.addAttribute("title", "Welcome to Pop!!");
+
+        return "";
     }
 
 }
