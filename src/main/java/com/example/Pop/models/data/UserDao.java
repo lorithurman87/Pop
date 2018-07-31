bd96cc57aa2b83c8b54141f48dc393b8588602f8
lorithurman87 <lori_thurman@rocketmail.com>
2018-07-30 19:38:50 -0500

    working on UserController and user dao trying to make that work. not finished

--- src/main/java/com/example/Pop/models/data/UserDao.java
+++ src/main/java/com/example/Pop/models/data/UserDao.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 //@Transactional
-public interface UserDao /*extends CrudRepository<User, Integer> */{
+public interface UserDao extends CrudRepository<User, Integer> {
 
     public List<User> findByUsername(String username);
 }
