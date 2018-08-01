0db10fcb82aa7e4ff33a14da14bb350d1ef50ab0
lorithurman87 <lori_thurman@rocketmail.com>
2018-07-31 19:58:00 -0500

    finished and about to test user signup and login

--- src/main/java/com/example/Pop/models/data/UserDao.java
+++ src/main/java/com/example/Pop/models/data/UserDao.java
@@ -2,18 +2,18 @@ package com.example.Pop.models.data;
 
 import com.example.Pop.models.User;
 import org.springframework.stereotype.Repository;
-//import javax.transaction.Transactional;
-//import org.springframework.data.repository.CrudRepository;
+import javax.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 
 @Repository
-//@Transactional
+@Transactional
 public interface UserDao extends CrudRepository<User, Integer> {
 
     public List<User> findByUsername(String username);
 }
-//public class UserDao { }
+
 
 
